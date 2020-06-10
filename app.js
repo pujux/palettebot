7b4f98fe1ee4429f0cebc78764d3232e317200af
Julian Pufler <puf17640@spengergasse.at>
2020-06-10 21:36:45 +0200

    lint my ass

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const AlphaFB = require('@alpha-manager/fb')
-const alpha = require('@alpha-manager/core')
-const Vibrant = require('node-vibrant')
+const AlphaFB = require('@alpha-manager/fb'),
+	alpha = require('@alpha-manager/core'),
+	Vibrant = require('node-vibrant'),
+	path = require('path')
+
 require('dotenv').config()
 const fb = new AlphaFB().config({
-  id: 311738659492506, 
-  token: process.env.TOKEN 
+	id: 311738659492506,
+	token: process.env.TOKEN
 })
 
 const { createCanvas, loadImage } = require('canvas')
@@ -12,25 +14,25 @@ const canvas = createCanvas(600, 700)
 const ctx = canvas.getContext('2d')
 
 new alpha.Task().to(fb).do(async actionObject => {
-  console.info(`${new Date().toISOString()} | starting post`)
-  ctx.drawImage((await loadImage('https://source.unsplash.com/random/600x600')), 0, 0, 600, 600)
-  const palette = await Vibrant.from(canvas.toBuffer()).getPalette()
-  ctx.fillStyle = '#fff'
-  ctx.fillRect(0, 600, 600, 100)
-  const keys = Object.keys(palette)
-  for(const i in keys) {
-    ctx.fillStyle = palette[keys[i]].getHex()
-    ctx.fillRect(45+i*60+i*30, 620, 60, 60)
-  }
-  const fs = require('fs')
-  const out = fs.createWriteStream(__dirname + '/test.jpeg')
-  out.on('finish', () => {
-    actionObject.type = 'post'
-    actionObject.message = `Colors extracted: ${Object.keys(palette).map(key => palette[key].getHex()).join(', ')}`
-    actionObject.media = __dirname + '/test.jpeg'
-    actionObject.done()
-    console.info(`${new Date().toISOString()} | posting finished`)
-    ctx.clearRect(0, 0, 600, 700)
-  })
-  canvas.createJPEGStream().pipe(out)
+	console.log(`${new Date().toISOString()} | starting post`)
+	ctx.drawImage(await loadImage('https://source.unsplash.com/random/600x600'), 0, 0, 600, 600)
+	const palette = await Vibrant.from(canvas.toBuffer()).getPalette()
+	ctx.fillStyle = '#fff'
+	ctx.fillRect(0, 600, 600, 100)
+	const keys = Object.keys(palette)
+	for (const i in keys) {
+		ctx.fillStyle = palette[keys[i]].getHex()
+		ctx.fillRect(45 + (i * 60) + (i * 30), 620, 60, 60)
+	}
+	const fs = require('fs')
+	const out = fs.createWriteStream(path.join(__dirname, '/test.jpeg'))
+	out.on('finish', () => {
+		actionObject.type = 'post'
+		actionObject.message = `Colors extracted: ${Object.keys(palette).map(key => palette[key].getHex()).join(', ')}`
+		actionObject.media = path.join(__dirname, '/test.jpeg')
+		actionObject.done()
+		console.log(`${new Date().toISOString()} | posting finished`)
+		ctx.clearRect(0, 0, 600, 700)
+	})
+	canvas.createJPEGStream().pipe(out)
 }).every(15).minute().start()
